4e831248dd94d6eda7f6a52fa46d2ba28ceafdd3
miu <38824651+miu200521358@users.noreply.github.com>
2023-12-24 15:22:36 +0900

    config

---
+++ mtest/frontend/src/components/pages/Config.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+// import { ref } from 'vue'
+</script>
+
+<template>
+  <h1>Config</h1>
+
+  <div class="card">
+    <p>
+      Edit
+      <code>components/pages/Config.vue</code> to test HMR
+    </p>
+  </div>
+</template>
